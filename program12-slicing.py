70cf676392b0d640f4c6f30ffaa0921ad88d2f79
vallapishettymythri <mythrivs123@gmail.com>
2025-09-28 21:42:33 +0530

    Slicing

---
+++ program12-slicing.py
@@ -0,0 +1,13 @@
+#Slicing-specifies start indexing and end indexing returns.
+#1)Both index
+b="Hello World!"
+print(b[2:5])
+
+#2)Slice from start
+print(b[2:])
+
+#3)Slice fron end
+print(b[2:])
+
+#4)Negative Indexing
+print(b[-5:-2])
