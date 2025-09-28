293e9778c1551ac9026ec0844ae2163b2e6f5958
vallapishettymythri <mythrivs123@gmail.com>
2025-09-28 21:36:52 +0530

    Basics of strings

---
+++ program11-strings.py
@@ -0,0 +1,21 @@
+#its all about the strings datatype.
+#1) Strings are arrays
+a="Hello World!"
+print(a[1])
+
+#2)Looping through string with a for loop
+for x in "banana":
+    print(x)
+
+#3)length of the string
+a="Hello World!"
+print(len(a))
+
+#4)Check string
+text="Hello World!"
+print("world!" in text)
+
+#5) check not in
+hey="Hello world!"
+print("is" not in hey)
+
