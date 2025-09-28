50af62f5eddaa61256a49bf600833dccf19b5e62
vallapishettymythri <mythrivs123@gmail.com>
2025-09-28 23:43:42 +0530

    Basic list methods

---
+++ program16-list.py
@@ -0,0 +1,23 @@
+#lists are used to store multiple values. It is one of the 4 built-in function.
+#It is used to store collections of data.
+#basic methods in list!
+
+#1) basic list
+thislist=["apple","mango","orange"]
+print(thislist)
+
+
+#2)length of the list
+print(len(thislist))
+
+#3) list items- Here in the list can contain different data types
+list1=["abc",34,True,40,"Male"]
+print(list1)
+
+#4)Type()-gives the data type of the objects like whether its list, tuple or dictionary. 
+print(type(list1))
+
+#5)list() constructor- constructor can be used to create a new list without using [].
+list2=list(("sun","moon","stars")) #double brackets are used
+print(list2)
+
