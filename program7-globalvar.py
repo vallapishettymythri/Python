614edd9bc4e004f3b75ea9cc455d71dce76a38e9
vallapishettymythri <mythrivs123@gmail.com>
2025-09-28 21:12:07 +0530

    Global variable

---
+++ program7-globalvar.py
@@ -0,0 +1,5 @@
+#variables that are created outside of a function. This variables are used by everyone both inside and the out side of function.
+x="Awesome"
+def myfunc():
+    print("python is " +x)
+myfunc()   #calling the function
