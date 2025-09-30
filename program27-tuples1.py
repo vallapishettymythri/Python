d3307cf56a80c17878c54571ba1c5deb7835e543
vallapishettymythri <mythrivs123@gmail.com>
2025-09-30 21:45:17 +0530

    Accesing tuple

---
+++ program27-tuples1.py
@@ -0,0 +1,15 @@
+#Acess tuple using index
+tuple=("apple","banana","cherry","kiwi","dragon","guava")
+print(tuple[1])
+
+print(tuple[-1])
+
+print(tuple[2:5])
+
+print(tuple[:4])
+
+print(tuple[2:])
+
+print(tuple[-4:-1])
+
+
