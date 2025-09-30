13da8cc2fe6e5198d46f675ac2d386b565e93950
vallapishettymythri <mythrivs123@gmail.com>
2025-09-30 21:38:03 +0530

    Tuple,len,type,duplicates

---
+++ program26-tuples.py
@@ -0,0 +1,16 @@
+#tuples: store multiple items in a single variable. 
+# It is collection which is ordered and unchangeable, and allows duplicates.
+tuple=("apple","banana","cherry")
+print(tuple)
+#it is represented using ()
+
+#allows duplicates
+tuple1=("apple","banana","cherry","banana","cherry")
+print(tuple1)
+
+#tuple length
+print(len(tuple1))
+
+#type
+print(type(tuple))
+
