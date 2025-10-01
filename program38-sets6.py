3ad3ba0006c05439cce98d329b69ce311a132191
vallapishettymythri <mythrivs123@gmail.com>
2025-10-01 13:47:56 +0530

    Frozenset

---
+++ program38-sets6.py
@@ -0,0 +1,9 @@
+#FrozenSet- Its an immutable version of sets. 
+#Unlike sets, elements or values can't be added or removed from frozen set.
+#To create a frozenset, we need to use frozenset() constructor.
+x=frozenset({"apple","kiwi","cherry"})
+print(x)
+print(type(x))
+
+#it has similar methods like sets. 
+#copy,difference,intersection,isdisjoin,issubset,issuperset,symmetric_difference,union.
