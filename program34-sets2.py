7d7bd40c08b7c9e1322eecada1bc3a8686d6304a
vallapishettymythri <mythrivs123@gmail.com>
2025-10-01 13:09:51 +0530

    Add items, add sets.

---
+++ program34-sets2.py
@@ -0,0 +1,12 @@
+#add items- we need add() method to add items into sets.
+thisset={"apple","banana","cherry"}
+thisset.add("Orange")
+print(thisset)
+
+#add sets-add items from one set to another set, update().
+tropical={"Mango","seethaphal"}
+thisset.update(tropical)
+print(thisset)
+
+#update() method is even used to add set to other built in types. 
+#not only set to set!
