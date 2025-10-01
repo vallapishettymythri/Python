ebda9068e6313a7243360926ef43396145a76369
vallapishettymythri <mythrivs123@gmail.com>
2025-10-01 11:19:56 +0530

    Join tuples and methods

---
+++ program31-tuples5.py
@@ -0,0 +1,13 @@
+#Join tuples- We use '+' operator
+tuple1=("apple","banana","cherry")
+tuple2=(1,2,3)
+tuple3=tuple1+tuple2
+print(tuple3)
+
+#muliply tuples
+mytuple=tuple1*2
+print(mytuple)
+
+#tuple methods- count()- counts occurance, index()-searches the value.
+
+
