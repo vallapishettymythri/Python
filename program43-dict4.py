973b2f2949ccbb739888aaad2713f42df52dfaef
vallapishettymythri <mythrivs123@gmail.com>
2025-10-03 11:20:06 +0530

    Loop through a dictionary

---
+++ program43-dict4.py
@@ -0,0 +1,12 @@
+#Loop through a dictionary
+#for loop
+thisdict={
+    "brand":"ford",
+    "model":"mustang",
+    "year" : 1964
+}
+for x in thisdict:
+    print(x)
+
+for x in thisdict:
+    print(thisdict[x])
