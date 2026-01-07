bc20efad556ed1991dca9a7486e226855ca82808
vallapishettymythri <mythrivs123@gmail.com>
2026-01-07 14:35:26 +0530

    Using math fucntions and modules

---
+++ program60-maths.py
@@ -0,0 +1,26 @@
+#unlike other languages python has built-in maths functions which are used directly.
+x=min(5,10,25)
+y=max(5,10,25)
+print(x,y)
+
+x=abs(-7.25)
+print(x)
+
+x=pow(4,3)
+print(x)
+
+#to use other things we can use math module. Which is automatic no need of other file creation.
+import math
+x=math.sqrt(64)
+print(x)
+
+
+import math
+x=math.ceil(1.4) #rounds upward value
+y=math.floor(1.4) #rounds downward value
+print(x)
+print(y)
+
+import math
+x=math.pi
+print(x)
