e170fc1c46ce569ac6e9f34d53618a49a404939f
vallapishettymythri <mythrivs123@gmail.com>
2026-01-08 11:52:04 +0530

    Basic of oops

---
+++ program64-oop.py
@@ -0,0 +1,9 @@
+#Object oriented programming allows you to structure your code using classes and objects for better organisation and reusability.
+#It provides clear structure, code easier, reuse and debug, avoids rewriting. 
+# It has class and object. 
+#A class defines a object and an object is created based on class.
+class myclass: #created a classs called my class and contains a variable x=5
+    x=5
+p1=myclass() #created an object to call myclass.
+print(p1.x) #printing p1 to print x value.
+
