93d1d27cf9cb24494c949c456f8b84b632361b8d
vallapishettymythri <mythrivs123@gmail.com>
2026-01-27 21:09:55 +0530

    Print Function

---
+++ hackerrank1.py
@@ -0,0 +1,12 @@
+#The included code stub will read an integer, , from STDIN.
+
+#Without using any string methods, try to print the following:
+
+
+#Note that "" represents the consecutive values in between.
+
+#Print the string 12345
+
+n = int(input())
+for i in range(1, n + 1):
+    print(i, end="")
