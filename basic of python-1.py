0fcadf4d7e0cfe636f5cb96a6a064219b3be8240
vallapishettymythri <mythrivs123@gmail.com>
2026-07-18 10:29:38 +0530

    coversion & 1's, 2's complement

---
+++ basic of python-1.py
@@ -0,0 +1,21 @@
+#Tokens: Tokens are the smallest individual units in the program. 
+# 1. Keywords- Reserved words/ predefined words. Whose meaning is automatically recognised by the complier. 
+import keyword
+print(keyword.kwlist) #prints all the important keywords. 
+
+# 2.Identifiers- are name we are using in program. Can be name of a bariable, function, class anything.
+#rules of the identifier:
+#1. It contains alphabets, numbers and special symbol called underscore(_).
+#2. Starts with a alphabet or the special character.
+#3. Should not be a keyword.
+#4. Case sensitive
+
+# 3. Operators- Special symbol which is used to perform the operations. 
+#On which operations are performed are called as operands.
+#i- Arithmatic operators- 
+#+-additing, --subtraction,*-multiplication,/-division, //-floor, %-modulus, **power.
+
+#Every operator as 2 things. 
+#1. Priority or precedence: 
+#Evaluating the expression using based on the priority of operators
+#2.Associativity: If two operators are having prioity are will use a associativity to solve expression. Like direction of solving left to right, right to left. 
