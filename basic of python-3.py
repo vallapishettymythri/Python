2b1714b2915931b6f6145afe71cd3e17d8e96303
vallapishettymythri <mythrivs123@gmail.com>
2026-07-18 10:03:25 +0530

    Basic of python- and, or, not

---
+++ basic of python-3.py
@@ -0,0 +1,11 @@
+print(34 and 54) #it gives the right most value)
+print(0 and 2) #gives default value which is 0
+print(0 and 0.0) #if the first expression has 0 then its default value is ouput. 
+print(0.0 and 2)
+
+
+print(12 or 14)#prints left most value
+print(0 or 5)#other the default value is output)
+
+print(not 0) #opposite of 0 is 1 is so its true
+print(not 6.7)#opposite of any number is 0 so its false
