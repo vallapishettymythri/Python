ddda1e173efc682cc63e4e191506d96e4c7c35d0
vallapishettymythri <mythrivs123@gmail.com>
2026-07-23 18:11:07 +0530

    Input output

---
+++ basic-ipop.py
@@ -0,0 +1,5 @@
+#input from the user is taken using input().
+#output from the user is taken using  print()
+#the program may or may not have input but has output. 
+#for input() we need to write which data type are we using. 
+a=int(input("a:"))#here only integer datatype is used. 
