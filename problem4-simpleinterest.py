506f72f50ca9d98561d76436e64df97fdb89cd81
vallapishettymythri <mythrivs123@gmail.com>
2026-07-24 14:45:46 +0530

    Simple interest

---
+++ problem4-simpleinterest.py
@@ -0,0 +1,7 @@
+#the formula so SI is ptr/100.
+#where p is principal amount, t- time taken, r-rate
+p=int(input("enter principle amount"))
+t=int(input("enter time"))
+r=int(input("enter rate"))
+SI=(p*t*r)/100
+print("Simple interest", SI)
