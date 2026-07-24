465aaefc68f58e23de0ffda105af4bb73b707954
vallapishettymythri <mythrivs123@gmail.com>
2026-07-24 14:38:58 +0530

    Area of equilateral triangle

---
+++ problem2-areaequilateral.py
@@ -0,0 +1,5 @@
+#equilateral triangle- all sides are equal. 
+#to find the area of equilateral triangle we use a formula.
+side=int(input("Enter the side of the triangle"))
+area=((3**0.5)/4)*(side**2)
+print("Area of equilateral triangle is:", area)
