a4785e06e4a5820745f072409194a5fd95ad4289
vallapishettymythri <mythrivs123@gmail.com>
2026-07-24 15:00:23 +0530

    Point from origin

---
+++ program6-ptorigin.py
@@ -0,0 +1,7 @@
+#so we need to find a point from origin. where x and y axis are present.
+#x1,y1 will be 0, as it is from origin. x2,y2 will be the input.
+#so we will only consider x and y.
+x=int(input("x value"))
+y=int(input("y value"))
+p=((x**2)+(y**2))**0.5
+print("Point",p)
