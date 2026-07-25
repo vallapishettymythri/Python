32319eff648d1c81634f2a3092b5cc4666c48e77
vallapishettymythri <mythrivs123@gmail.com>
2026-07-25 17:56:17 +0530

    Signum function implementation

---
+++ problem23-signum.py
@@ -0,0 +1,11 @@
+#Signum Function Implementation
+#Write a Python code to read the value of an integer m and display the value of
+#n is 1 when m is larger than 0, n is 0 when m is 0 and n is -1 when m is less than 0.
+m=int(input("Enter number:"))
+if m>0:
+    print("The value of n=1")
+elif m==0:
+    print("the value of n=0")
+else:
+    print("the value of n=-1")
+    
