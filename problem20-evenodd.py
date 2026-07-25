2b9cdceee57528444f77f212b8babf97f3e5bcf0
vallapishettymythri <mythrivs123@gmail.com>
2026-07-25 17:52:53 +0530

    Even or odd

---
+++ problem20-evenodd.py
@@ -0,0 +1,8 @@
+#Even or Odd Check
+#Write a Python code to check whether a given number is even or odd.
+a=int(input("Enter number:"))
+if a%2==0:
+    print(a,"is an odd integer")
+else:
+    print(a,"is not an odd integer")
+    
