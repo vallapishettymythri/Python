0be7a70a7c44528b9eb18b26e6904f2dd03057da
vallapishettymythri <mythrivs123@gmail.com>
2026-07-25 17:51:28 +0530

    two integers and check whether they are equal or not

---
+++ problem19-equalitycheck.py
@@ -0,0 +1,8 @@
+#Equality Check
+#Write a Python code to accept two integers and check whether they are equal or not.
+Number1=int(input("First number:"))
+Number2=int(input("Second number:"))
+if Number1==Number2:
+    print("Number1 and Number2 are equal")
+else:
+    print("Number 1 and number2 are not equal")
