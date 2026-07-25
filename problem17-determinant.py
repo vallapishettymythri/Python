1a1a8591d88a0b98988479ae6a7782c532189e29
vallapishettymythri <mythrivs123@gmail.com>
2026-07-25 16:56:35 +0530

    Determinant of a matrix

---
+++ problem17-determinant.py
@@ -0,0 +1,8 @@
+#WAP to find determinant
+x1=int(input("x1:"))
+x2=int(input("x2:"))
+y1=int(input("y1:"))
+y2=int(input("y2:"))
+d=x1*(y2-1)-x2*(y1-1)+1*(y1-y2)
+print('Determinant:', d)
+#determinant is nothing but the matrix form to calculate. 
