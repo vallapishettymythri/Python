149745802942ed987fd816b272b4cc1a11487a87
vallapishettymythri <mythrivs123@gmail.com>
2026-07-25 17:55:01 +0530

    Check voting eligibility

---
+++ problem22-voting.py
@@ -0,0 +1,7 @@
+#Voting Eligibility
+#Write a Python code to read the age of a candidate and determine whether he is eligible to cast his/her own vote.
+a=int(input("Enter number:"))
+if a>=18:
+    print("Congratulation! You are eligible for casting your vote.")
+else:
+    print("Not eligible to cast your vote.")
