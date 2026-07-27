70272af1e1dd63a91ebae8ded905cfff90ee722a
vallapishettymythri <mythrivs123@gmail.com>
2026-07-27 15:40:31 +0530

    Perimeter of circle

--- problem3-permetercir.py
+++ problem3-permetercir.py
@@ -1,4 +1,4 @@
 #perimeter of circle is 2pir. Where the pi value is 2.14.
 radius=int(input("Enter the radius of the circle:"))
-circum=2*2.14*radius
+circum=2*3.14*radius
 print("Perimeter of circle:", circum)
