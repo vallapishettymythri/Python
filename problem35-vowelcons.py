b240efd1fd25d11d33a5944358f8ca2e49e90929
vallapishettymythri <mythrivs123@gmail.com>
2026-07-28 12:47:11 +0530

    Vowel consonant check

---
+++ problem35-vowelcons.py
@@ -0,0 +1,8 @@
+#Vowel or Consonant Check
+#Write a Python code to check whether an alphabet is a vowel or a consonant.
+al=input("Enter alph:")
+if (al=='a') or (al=='e') or (al=='i') or (al=='o') or (al=='u'):
+    print("Vowel")
+else:
+    print("Consonant")
+    
