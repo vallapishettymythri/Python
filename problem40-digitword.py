9e00c05d35d773af7151f7d2db086413dc32f67b
vallapishettymythri <mythrivs123@gmail.com>
2026-07-28 14:31:23 +0530

    display a digit in words

---
+++ problem40-digitword.py
@@ -0,0 +1,26 @@
+# Program to display a digit in words
+
+digit = int(input("Enter a digit (0-9): "))
+
+if digit == 0:
+    print("Zero")
+elif digit == 1:
+    print("One")
+elif digit == 2:
+    print("Two")
+elif digit == 3:
+    print("Three")
+elif digit == 4:
+    print("Four")
+elif digit == 5:
+    print("Five")
+elif digit == 6:
+    print("Six")
+elif digit == 7:
+    print("Seven")
+elif digit == 8:
+    print("Eight")
+elif digit == 9:
+    print("Nine")
+else:
+    print("Invalid Digit")
