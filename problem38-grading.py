79b4248de04432a1a12b194adead279f94d2929c
vallapishettymythri <mythrivs123@gmail.com>
2026-07-28 12:58:06 +0530

    Grade to description mapping

---
+++ problem38-grading.py
@@ -0,0 +1,16 @@
+#Grade to Description Mapping
+#Write a program in Python to accept a grade and declare the equivalent
+#description :
+a=input("select from E,V,G,A,F:")
+if a=='E':
+    print("Excellent")
+elif a=='V':
+    print("Very good")
+elif a=='G':
+    print("Good")
+elif a=='A':
+    print("Average")
+elif a=='F':
+    print("Fail")
+else:
+    print("Unknown grade")
