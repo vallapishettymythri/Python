36b1ee545a3d26def978f765cc351a760d6e54aa
vallapishettymythri <mythrivs123@gmail.com>
2026-08-05 11:55:54 +0530

    Rotate  k value of number using while loop where k is greater than digits

---
+++ rotateknumdigi.py
@@ -0,0 +1,15 @@
+#rotate k value of number using while loop where k is greater then digit.
+n=int(input("enter values:"))
+k=int(input("k:"))
+temp=n
+count=0
+while temp>0:
+    count+=1
+    temp//=10
+k%=count #if k value is greater than digits we use this
+last=n%(10**k)
+first=n//(10**k)
+shift=10**(count-k)
+answer=last*shift+first
+print(answer)
+    
