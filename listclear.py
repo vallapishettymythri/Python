ff92f4086c594791df83c3d80819f953f094c441
vallapishettymythri <mythrivs123@gmail.com>
2026-08-23 13:53:35 +0530

    Clear function in list

---
+++ listclear.py
@@ -0,0 +1,10 @@
+#clear()-clears te entire list
+lst=[23,11,-22,56,12,67,99,3]
+lst.clear()
+lst
+
+# Clear using function
+def myclear(lst):
+    return []
+lst=[23,11,-22,56,12,67,99,3]
+myclear(lst)
