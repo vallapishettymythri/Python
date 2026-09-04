5ee8e3b31e695036a5d4e65a628f4b77ca55d0d5
vallapishettymythri <mythrivs123@gmail.com>
2026-09-04 19:17:43 +0530

    Day to Activity planner

---
+++ dayactivityplanner.py
@@ -0,0 +1,20 @@
+#Day-to-Activity Planner
+# Write your code here
+n=input()
+match n:
+    case "Monday":
+        print("Go for a run")
+    case "Tuesday":
+        print("Attend a cooking class")
+    case "Wednesday":
+        print("Work on a personal project")
+    case "Thursday":
+        print("Watch a movie")
+    case "Friday":
+        print("Hang out with friends")
+    case "Saturday":
+        print("Go shopping")
+    case "Sunday":
+        print("Relax at home")
+    case _: 
+        print("Invalid day input.")
