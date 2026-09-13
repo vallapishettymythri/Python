2eb9d850bc3c7d1bafeedfea166ed8698563548a
vallapishettymythri <mythrivs123@gmail.com>
2026-09-13 16:12:48 +0530

    regex characters- fullmatch(),{},\s,^,$

---
+++ regexchar6.py
@@ -0,0 +1,21 @@
+#fullmatch()- checks if the whole string matches
+
+#WAP to check whether a string contains all digits or not
+import re
+def digits(string):
+    pattern="\d+"
+    match = re.fullmatch(pattern, string)
+    if match:
+        return True
+    else:
+        return False
+string="123456789g"
+digits(string)
+
+#{}- quantifiers- tells how many times a character/pattern should occur.
+
+#\s- spaces
+
+#^-start of the string
+
+#$- end of the string
