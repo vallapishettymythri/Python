17c33fabebf6fee8a5dd4fbc4b112d8899586522
vallapishettymythri <mythrivs123@gmail.com>
2026-09-26 10:13:13 +0530

    Single inheritance basic way

---
+++ basic_single_inheritance.py
@@ -0,0 +1,10 @@
+#Single inheritance
+class base_class:
+    def __init__(self):
+        print("parent class constructor")
+    def display(self):
+        print("thank you!!")
+class derived_class(base_class):
+    def display2(self):
+        print("hello")
+obj=derived_class()
